cadfd0e1dc1a00dfa95ffcaabe864ce461e7ad74
Aniket Magadum <aniketmagadum@fytinnovations.com>
2019-08-31 12:46:18 +0530

    Completed with weekly Line graph

--- controllers/Subscribers.php
+++ controllers/Subscribers.php
@@ -23,15 +23,17 @@ class Subscribers extends \Backend\Classes\Controller {
         $this->vars['unverified_subscribers']=Subscriber::unverified()->count();
         $weekly_subcribers= DB::table('fytinnovations_userconnect_subscribers')
                  ->select(DB::raw('UNIX_TIMESTAMP(created_at) as date,count(*) as total'))
+                 ->where('created_at','>',date('Y-m-d', strtotime('-7 days')))
                  ->groupBy(DB::raw('date(created_at)'))
+                 ->orderBy(DB::raw('date(created_at)'))
                  ->get();
-
+        //dd($weekly_subcribers);
         $graph_array=[];
         foreach ($weekly_subcribers as $key => $subcriber) {
-            $array=[$subcriber->date,$subcriber->total];
+            $array=[$subcriber->date*1000,$subcriber->total];
             array_push($graph_array,$array);
         }
-
+        
         //Convert the array into a string which can be passed to the view graph
         $this->vars['line_graph']=substr(json_encode($graph_array), 1, -1);
         //dd($this->vars['line_graph']);
