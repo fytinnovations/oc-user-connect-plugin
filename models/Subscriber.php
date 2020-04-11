ac95dfc48db0b47fd35cabe986d5c85159a73316
Aniket Magadum <aniketmagadum77@gmail.com>
2020-04-11 16:25:05 +0530

    Modify SubscriptionForm and Subscriber Model

--- models/Subscriber.php
+++ models/Subscriber.php
@@ -1,7 +1,10 @@
-<?php namespace Fytinnovations\UserConnect\Models;
+<?php
+
+namespace Fytinnovations\UserConnect\Models;
 
 use Model;
-use Illuminate\Support\Str;
+use Event;
+use Mail;
 
 /**
  * Subscribers Model
@@ -15,7 +18,8 @@ class Subscriber extends Model
     public $table = 'fytinnovations_userconnect_subscribers';
 
     public $rules = [
-        'email'    => 'required|between:6,255|email',
+        'email'    => 'required|between:6,255|email|unique:fytinnovations_userconnect_subscribers',
+        'verification_key' => 'nullable|unique:fytinnovations_userconnect_subscribers',
     ];
 
     /**
@@ -26,7 +30,7 @@ class Subscriber extends Model
     /**
      * @var array Fillable fields
      */
-    protected $fillable = ['email','verification_key','is_verified'];
+    protected $fillable = ['email', 'verification_key', 'is_verified'];
 
     protected $dates = ['verified_at'];
     /**
@@ -57,7 +61,66 @@ class Subscriber extends Model
     }
     public function scopeWeeklySubscribers($query)
     {
-        $start_date = date("Y-m-d",strtotime("-1 week +1 day"));
-        return $query->where('created_at','>', $start_date)->groupBy(DB::raw('date(created_at)'))->count();
+        $start_date = date("Y-m-d", strtotime("-1 week +1 day"));
+        return $query->where('created_at', '>', $start_date)->groupBy(DB::raw('date(created_at)'))->count();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     */
+    public function beforeCreate()
+    {
+        $settings = Settings::instance();
+
+        $isVerificationEnabled = $settings->verify_emails;
+
+        if ($isVerificationEnabled) {
+            $keyExpiresAfter = $settings->key_expires_in;
+
+            $this->verification_key = static::generateVerificationKey();
+            $this->valid_till = date('Y-m-d H:i:s', strtotime('+' . $keyExpiresAfter . ' day', time()));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     */
+    public function afterCreate()
+    {
+        $isVerificationEnabled = Settings::get('verify_emails');
+
+        if ($isVerificationEnabled) {
+
+            $vars = [
+                'link' => url('/email_verification/' . $this->email . '/' . $this->verification_key),
+                "app_name" => config('app.name')
+            ];
+
+            Mail::send('fytinnovations.userconnect::mail.verify_subscriber', $vars, function ($message) {
+                $message->to($this->email);
+            });
+        }
+    }
+
+    /**
+     * Generates a unique uuid for verification of the subscriber
+     *
+     * @return string
+     */
+    protected static function generateVerificationKey()
+    {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
     }
 }
