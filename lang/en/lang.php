4db42c531e305d28368679e1197f8369fe8e1813
Aniket Magadum <aniketmagadum77@gmail.com>
2020-04-11 16:25:05 +0530

    Modify SubscriptionForm and Subscriber Model

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -20,6 +20,18 @@ return [
         'subscriptionform' => [
             'name' => 'Subscription Form',
             'description' => 'Displays a form through which users can subscribe to mailing lists',
+            'properties' => [
+                'subscribeButtonText' => [
+                    'title' => 'Subscribe button text',
+                    'description' => 'The text to be displayed on the subscribe button.',
+                    'default' => 'Subscribe'
+                ],
+                'successMessage' => [
+                    'title' => 'Success Message',
+                    'description' => 'Text to be displayed when the subscription is sucessful.',
+                    'default' => 'Thankyou for subscribing.'
+                ]
+            ]
         ],
     ],
 ];
